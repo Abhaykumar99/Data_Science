f0660479fea6cab20f2c9271673bbcc10d8132ea
Abhay Kumar <krabhi4201@gmail.com>
2026-02-01 23:55:42 +0530

    Initial Commit

--- 31_WorkingWithCSVs.py
+++ 31_WorkingWithCSVs.py
@@ -1,3 +1,15 @@
 import pandas as pd
 df=pd.read_csv("sample_dataset.csv")
 print(df)
+print(df.dtypes)
+print(list(df.columns))
+print(df["Marks"].max())
+
+df=df[df["Marks"]>80]
+print(df)
+df.to_csv("updated.csv",index=False)
+
+df1=pd.read_json("data.json")
+print(df1)
+df1=pd.json_normalize(df1)
+print(df1)
